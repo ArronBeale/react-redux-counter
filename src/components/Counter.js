329f26a294f10eae6fbaedb178de80c2cb5e03d6
Arron <beale86@msn.com>
2023-08-01 07:30:39 +0000

    Add class based component alternative setup, commented out

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,20 @@
-import { useSelector, useDispatch } from 'react-redux';
+// Import for Class based component
+import { Component } from 'react';
+
+import { useSelector, useDispatch, connect } from 'react-redux'; // Class based comps cannot use hooks so use connect
 
 import classes from './Counter.module.css';
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector(state => state.counter);
+  const counter = useSelector((state) => state.counter);
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' })
+    dispatch({ type: 'increment' });
   };
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' })
+    dispatch({ type: 'decrement' });
   };
 
   const toggleCounterHandler = () => {};
@@ -29,4 +32,44 @@ const Counter = () => {
   );
 };
 
+// Class based component
+// class Counter extends Component {
+//   incrementHandler() {
+//     this.props.increment();
+//   }
+
+//   decrementHandler() {
+//     this.props.decrement();
+//   }
+
+//   toggleCounterHandler() {}
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//           <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return { counter: state.counter };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({ type: 'increment'}),
+//     decrement: () => dispatch({ type: 'decrement'}),
+//   };
+// };
+
 export default Counter;
+// Class based component export using connect, a higher order component
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
