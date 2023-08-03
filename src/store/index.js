c13491c6c84a973c8f6bddec11c74f7a56dcbdd5
Arron <beale86@msn.com>
2023-08-03 07:28:03 +0000

    Install redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,37 +1,40 @@
-import { createStore} from 'redux';
+import { createStore } from 'redux';
 
 const initialState = { counter: 0, showCounter: true };
 
 const counterReducer = (state = initialState, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-        };
-    }
+  if (action.type === 'increment') {
+    /* NEVER do this, never mutate the existing state 
+         state.counter++*/
 
-    if (action.type === 'increase') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
-        };
-    }
+    return {
+      counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
+  }
 
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        };
-    }
+  if (action.type === 'increase') {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
 
-    if (action.type === 'toggle') {
-        return {
-            showCounter: !state.showCounter,
-            counter: state.counter
-        };
-    }
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
 
-    return state;
+  if (action.type === 'toggle') {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
+    };
+  }
+
+  return state;
 };
 
 const store = createStore(counterReducer);
